d180119b81b3f28f390f05cc936ca97868bde4b6
Derek Hamilton <dhamiltonstl@gmail.com>
2024-03-26 14:50:22 -0500

    Completed boolean and conditional exercise

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,5 +1,13 @@
 // Declare and initialize the variables for exercise 1 here:
-
+let engineIndicatorLight = "green";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady
+let computerStatusCode = 200;
+let shuttleSpeed = 15000
+let fuelLevel = 20001
+let engineTemperature = 2500
+let commandOverride = false
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
@@ -9,3 +17,25 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+if (crewStatus == true) console.log("Crew Ready")
+else console.log("Crew Not Ready")
+
+if (computerStatusCode == 200) console.log("Please stand by. Computer is rebooting.")
+else if (computerStatusCode == 400) console.log("Success! Computer online.")
+else console.log("ALERT: Computer offline!")
+
+if (shuttleSpeed > 17500) console.log("ALERT: Escape velocity reached!")
+else if (shuttleSpeed < 8000) console.log("ALERT: Cannot maintain orbit!")
+else console.log("Stable speed")
+
+
+if (fuelLevel < 1000 || engineTemperature > 2500 || engineIndicatorLight == "red blinking") console.log("ENGINE FAILURE IMMINENT!")
+else if (fuelLevel > 20000 && engineTemperature <= 2500) console.log("Full tank. Engines good.")
+else if (fuelLevel > 10000 && engineTemperature <= 2500) console.log("Fuel level above 50%. Engines good.")
+else if (fuelLevel > 5000 && engineTemperature <= 2500) console.log("Fuel level above 25%. Engines good.")
+else if (fuelLevel <= 5000 || engineTemperature > 2500) console.log("Check fuel level. Engines running hot.")
+else console.log("Fuel and engine status pending...")
+
+if (fuelLevel > 20000 && engineIndicatorLight !== "red blinking" || commandOverride) console.log("Cleared to launch!")
+else console.log("Launch scrubbed!")
