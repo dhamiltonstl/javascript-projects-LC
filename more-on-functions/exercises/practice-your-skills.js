b8db7551e748279980f659a43644ce808b660a64
Derek Hamilton <dhamiltonstl@gmail.com>
2024-04-02 15:46:52 -0500

    more func exercise 1 done

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,7 +5,13 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
-
+let anonFunc = function(input) {
+    for (i in input) {
+        if (typeof input[i] === "number") input[i] *= 3
+        else if (typeof input[i] === "string") input[i] = "ARRR!"
+    }
+    return console.log(input)
+}
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +19,5 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+anonFunc(arr)
