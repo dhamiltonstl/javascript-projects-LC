62f9bf08c5596f90f37b4bf6d3108a82f2bdc479
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-04-25 11:42:21 -0500

    completed form exercise

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,23 @@
 //Code Your Solution Below
+window.addEventListener("load", e => {
+    let form = document.querySelector("form")
+    form.addEventListener("submit", e => {
+        let testName = document.querySelector("input[name=testName]")
+        let testDate = document.querySelector("input[name=testDate]")
+        let rocketType = document.querySelector("input[name=rocketType]")
+        let boosterCount = document.querySelector("input[name=boosterCount]")
+        let windRating = document.querySelector("input[name=windRating]")
+        let productionServers = document.querySelector("input[name=productionServers]")
+        if (
+            testName.value === "" ||
+            testDate.value === "" ||
+            rocketType.value === "" ||
+            boosterCount.value === "" ||
+            windRating.value === "" ||
+            productionServers.value === ""
+        ) {
+            alert("All fields are required!")
+            event.preventDefault()
+        }
+    })
+})
